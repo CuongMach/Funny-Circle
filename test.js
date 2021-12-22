8e251bdaebc8ac7e91a1ddef14ed35022abb3170
CuongMach <tuancuong682003@gmail.com>
2021-12-22 12:10:11 +0700

    funny circle

---
+++ test.js
@@ -0,0 +1,18 @@
+function Circle(x, y, radius){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+}
+
+function getRandomHex(){
+    return Math.floor(Math.random()*255);
+}
+
+function getRandomColor(){
+    var red = getRandomHex();
+    var green = getRandomHex();
+    var blue = getRandomHex();
+    return "rgb(" + red + "," + blue + "," + green +")";
+}
+
+
